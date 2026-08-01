fe3f19aa2d941b53dfc55ba132439df92fc3d424
Edilberto Rodriguez <85198098+edilbertorodriguez@users.noreply.github.com>
2026-08-01 22:23:20 +0000

    feat: refactor resources into reusable common module

--- environments/production/manifests/site.pp
+++ environments/production/manifests/site.pp
@@ -1,11 +1,3 @@
 node default {
-  package { 'htop':
-    ensure => installed,
-  }
-
-  file { '/tmp/puppet-managed.txt':
-    ensure  => file,
-    content => "Managed by Puppet\n",
-    mode    => '0644',
-  }
+  include common
 }
